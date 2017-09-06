711b76c99d97e351135ceb47c925b82411de8dc5
miha2158 <miha.vanya@gmail.com>
2017-09-06 21:21:15 +0500

    Начало положено

---
+++ Task9/TwoWayLinkedList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task9
+{
+    class TwoWayLinkedList
+    {
+        public int Data { get; private set; } = 0;
+        public TwoWayLinkedList next { get; private set; } = null;
+        public TwoWayLinkedList previous { get; private set; } = null;
+
+    }
+}
